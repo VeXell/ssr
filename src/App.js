0a121e40020536e0249f216eb20ca96756fdf47b
vexell <vexell@gmail.com>
2020-01-23 15:36:16 +0000

    First structure

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/public/assets/logo.svg" className="App-logo" />
+        <p>
+          Edit! <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={event => {
+            event.preventDefault();
+            alert('123');
+          }}
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
