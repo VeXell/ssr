59dc7b0641685c15586effc19bd870dc4283e70b
vvolkov <v.volkov@corp.badoo.com>
2020-01-25 00:51:38 +0000

    Fix modules

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -40,7 +40,7 @@ module.exports = {
                                 loader: 'css-loader',
                                 options: {
                                     importLoaders: 1,
-                                    modules: false,
+                                    modules: true,
                                 }
                             },
                             'postcss-loader'
